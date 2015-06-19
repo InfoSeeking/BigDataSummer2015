5775d79e7d589a1283458d4f34a7a79958f3bd0a
Bhagyashree <padalkarbhagyashree@gmail.com>
2015-06-19 13:20:37 +0530

    Update tagger.py

--- tagger.py
+++ tagger.py
@@ -12,6 +12,7 @@ def _split_results(rows):
         if len(line) > 0:
             if line.count('\t') == 2:
                 parts = line.split('\t')
+#		return parts
                 tokens = parts[0]
                 tags = parts[1]
                 confidence = float(parts[2])
@@ -33,28 +34,17 @@ def _call_runtagger(tweets, run_tagger_cmd=RUN_TAGGER_CMD):
     return pos_results
 
 
-def runtagger_parse(tweets, run_tagger_cmd=RUN_TAGGER_CMD):
-    """Call runTagger.sh on a list of tweets, parse the result, return lists of tuples of (term, type, confidence)"""
-    pos_raw_results = _call_runtagger(tweets, run_tagger_cmd)
-    pos_result = []
-    for pos_raw_result in pos_raw_results:
-        pos_result.append([x for x in _split_results(pos_raw_result)])
-    return pos_result
-
-if __name__ == "__main__":
-	reload(sys)  
-	sys.setdefaultencoding('utf8')
-	with open('Modi.csv', 'rb') as f:
+def runtagger_parse(fname, run_tagger_cmd=RUN_TAGGER_CMD):
+	with open(fname, 'rb') as f:
 		reader = csv.reader(f)
-  		mylist = list(reader)
+		mylist = list(reader)
 	f.close()
 	tweets=[]		
 	for row in mylist:
 		tweets.append(str(row[0]))	
-	#print tweets
-	pos_tweets=runtagger_parse(tweets)
-	with open('TaggedModi.csv','a') as f:	
-		wr = csv.writer(f)
-		wr.writerows(pos_tweets)
-	f.close()
-	print("Done")
+	pos_raw_results = _call_runtagger(tweets, run_tagger_cmd)
+    	pos_result = []
+    	for pos_raw_result in pos_raw_results:
+        	pos_result.append([x for x in _split_results(pos_raw_result)])
+    	return pos_result
+	
